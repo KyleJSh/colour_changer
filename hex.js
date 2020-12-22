3e34700d137866c4294b0acc87e1f3e3ccc4bbbc
Kyle Sherrington <kylejsherrington@gmail.com>
2020-12-22 13:24:54 -0800

    changed some CSS, fixed JS error

--- hex.js
+++ hex.js
@@ -1,18 +1,18 @@
-ethereum.autoRefreshOnNetworkChange = false;
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]; // letters represent 10-15
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
-
-btn.addEventListener('click', function() {
-  let hexColor = '#' // this must be at the beginning of each color. Has to be let/var because const will give an error, as it can't be updated
-  for(let i = 0; i < 6; i++){
-    hexColor += hex[getRandomNumber()]
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
   }
+  // console.log(hexColor);
+
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
 });
 
-function getRandomNumber(){
+function getRandomNumber() {
   return Math.floor(Math.random() * hex.length);
 }
